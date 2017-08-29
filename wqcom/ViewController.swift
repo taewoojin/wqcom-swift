3c2d89c7fb87da4019c562434f8bb8cb5703cf15
Taewoo <qkqnrpa@gmail.com>
2017-08-29 13:11:38 +0900

    Initial Commit

---
+++ wqcom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wqcom
+//
+//  Created by 진용호 on 2017. 8. 29..
+//  Copyright © 2017년 jjlee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
